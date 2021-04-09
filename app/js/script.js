fd85799d188bc5238631140a8c08748891a9f96c
raki893 <raki893@gmail.com>
2021-04-09 11:57:28 +0600

    Designed menu animation

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,12 @@
-console.log('hello world!');
+const btnhamburger = document.querySelector('#btnhamburger');
+
+btnhamburger.addEventListener('click', function(){
+    console.log('click hamburger');
+
+    if(btnhamburger.classList.contains('open')){
+        btnhamburger.classList.remove('open');
+    }
+    else {
+        btnhamburger.classList.add('open');
+    }
+});
