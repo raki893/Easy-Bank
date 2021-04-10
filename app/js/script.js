9dd56ce2f7a24e1347247bf9ca7eea091c670814
raki893 <raki893@gmail.com>
2021-04-10 10:44:19 +0600

    Completed Part-2

--- app/js/script.js
+++ app/js/script.js
@@ -1,12 +1,18 @@
 const btnhamburger = document.querySelector('#btnhamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+
 
 btnhamburger.addEventListener('click', function(){
-    console.log('click hamburger');
 
-    if(btnhamburger.classList.contains('open')){
-        btnhamburger.classList.remove('open');
+    if(header.classList.contains('open')){      // Close menu
+        header.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
     }
-    else {
-        btnhamburger.classList.add('open');
+    else {      // Open menu
+        header.classList.add('open');
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
     }
 });
